f5670af17d3c38641bc817efef7e02feef900765
cosr2996 <115452805+cosr2996@users.noreply.github.com>
2023-05-03 11:56:43 -0600

    se agrego el boton para agregar un gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import Header from './components/header'
- 
+import { useState } from "react";
+import Header from "./components/header";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
-
-  const [presupuesto,setPresupuesto] = useState(0)
-  const [isValidPresupuesto,setIsValidPresupuesto] =  useState(false)
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   return (
     <div>
       <Header
-        presupuesto ={presupuesto}
+        presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
-      
+
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img src={IconoNuevoGasto} alt="icono nuevo gasto" />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
