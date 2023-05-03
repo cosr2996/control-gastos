69b80540d0eeed52f4c367fc27da93e224e3cf94
cosr2996 <115452805+cosr2996@users.noreply.github.com>
2023-05-03 11:43:00 -0600

    se agrego una funcion para formatear el prespuesto en un texto mas entendible USD

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 export const ControlPresupuesto = ({presupuesto}) => {
+
+        const formatearCantidad =(cantidad)=>{
+            return cantidad.toLocaleString('en-US',{
+                style:'currency',
+                currency:'USD'
+            })
+        }
+    
+
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         <div>
@@ -8,7 +17,7 @@ export const ControlPresupuesto = ({presupuesto}) => {
         </div>
         <div className='contenido-presupuesto'>
             <p>
-                <span>Presupuesto: </span>${presupuesto}
+                <span>Presupuesto: </span>{formatearCantidad(presupuesto)}
             </p>
         </div>
     </div>
